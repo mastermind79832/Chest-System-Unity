15712f466c6a0e0f39e5eb340f434358f8156a38
Aditya E Ajith <adityaeajith@gmail.com>
2022-07-30 20:03:33 +0530

    Created Chest
    - Created Chest type
    - Created Chest Controller

---
+++ Chest System/Assets/_Project/Scripts/Chests/ChestController.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ChestSystem.UI;
+
+namespace ChestSystem.Chest
+{
+    public enum ChestState
+	{
+        Locked = 0,
+        Unlocking = 1,
+        Unlocked = 2,
+	}
+
+	public class ChestController : MonoBehaviour
+	{
+		[Header("ChestVisual")]
+		[SerializeField]
+		private Image m_ChestTop;
+		[SerializeField]
+		private Image m_Chestbottom;
+
+        private ChestSO chestModel;
+
+		private float m_PrevTime; // used to store the time just before decreasing the timer
+        private float m_Timer;
+		public Action<int> OnTimerUpdated;
+
+        private ChestState m_State;
+		public ChestState State { get { return m_State; } }
+
+		private void Start()
+		{
+			ChangeState(ChestState.Locked);
+			m_ChestTop.sprite = null;
+			m_Chestbottom.sprite = null;
+		}
+
+		public void Initialize(ChestSO newChestType)
+		{
+			chestModel = newChestType;
+			ChangeState(ChestState.Locked);
+			m_Timer = chestModel.UnlockTime;
+			m_PrevTime = m_Timer;
+			InitChest();
+		}
+
+		private void InitChest()
+		{
+			m_ChestTop.sprite = chestModel.TopSprite;
+			m_Chestbottom.sprite = chestModel.BottomSprite;
+		}
+
+		public void StartUnlocking()
+		{
+            ChangeState(ChestState.Unlocking);
+		}
+
+		private void Update()
+		{
+			if (m_State == ChestState.Unlocking)
+			{
+				DecreaseTimer();
+				if (IsTimerOver())
+					ChangeState(ChestState.Unlocked);
+			}
+		}
+
+		private void DecreaseTimer()
+		{
+			m_Timer -= Time.deltaTime;
+			
+			if((m_Timer - m_PrevTime) > 1)
+			{
+				m_PrevTime = m_Timer;
+				OnTimerUpdated((int)m_Timer);
+			}
+		}
+		private bool IsTimerOver() => m_Timer <= 0;
+		private void ChangeState(ChestState state) => m_State = state;
+
+		public void Open(out int coinAmount, out int gemAmount)
+		{
+			coinAmount = UnityEngine.Random.Range(chestModel.CoinRange.Min, chestModel.CoinRange.Max);
+			gemAmount = UnityEngine.Random.Range(chestModel.GemRange.Min, chestModel.GemRange.Max);
+		}
+
+		public void ResetChest()
+		{
+			chestModel = null;
+			m_ChestTop.sprite = null;
+			m_Chestbottom.sprite = null;
+		}
+	}
+}
---
+++ Chest System/Assets/_Project/Scripts/Chests/ChestSO.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChestSystem.Chest
+{
+	[System.Serializable]
+    public struct RangePacketInt
+	{
+        public int Min;
+        public int Max;
+	}
+
+	[CreateAssetMenu(menuName = "Chest/New Chest", fileName = "New Chest Type")]
+    public class ChestSO : ScriptableObject
+    {
+		[Header("Goodies")]
+        public RangePacketInt CoinRange;
+        public RangePacketInt GemRange;
+
+		[Header("Time")]
+		[Tooltip("Time needed to unlock in sec")]
+        public float UnlockTime;
+
+		[Header("Image")]
+        public Sprite TopSprite;
+        public Sprite BottomSprite;
+    }
+}
